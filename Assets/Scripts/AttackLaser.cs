53883caab16e2a754d9591c4f4c115ee55217a79
bnagooyen <briannguyen33@gmail..com>
2016-10-12 23:07:21 -0700

    added pointer behavior, accesses reference to target

---
+++ Assets/Scripts/AttackLaser.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(ControllerEvents))]
+[RequireComponent(typeof(LaserPointer))]
+
+public class AttackLaser : MonoBehaviour {
+
+    private LaserPointer laserPointer;
+    private ControllerEvents controllerEvents;
+
+    private Color laserDefaultColor;
+    private float damagePerFrame = 100f;
+    private bool damage = false;
+
+    //need a reference for what is being targeted/attacked 
+    private EnemyHealth targetHealth;
+
+	void Awake () {
+        laserPointer = GetComponent<LaserPointer>();
+        controllerEvents = GetComponent<ControllerEvents>();
+
+        laserDefaultColor = laserPointer.color;
+        print(laserDefaultColor);
+	}
+	
+	
+    void OnEnable()
+    {
+        controllerEvents.TriggerPressed += HandleTriggerPress;
+        controllerEvents.TriggerReleased += HandleTriggerRelease;
+
+    }
+
+    void OnDisable()
+    {
+        controllerEvents.TriggerPressed -= HandleTriggerPress;
+        controllerEvents.TriggerReleased -= HandleTriggerRelease;
+
+    }
+
+    private void HandleTriggerPress(object sender, ControllerEvents.ControllerInteractionEventArgs e)
+    {
+        laserPointer.enabled = true;
+        laserPointer.PointerIn += HandlePointerIn;
+        laserPointer.PointerOut += HandlePointerOut;
+    }
+
+   
+
+    private void HandleTriggerRelease(object sender, ControllerEvents.ControllerInteractionEventArgs e)
+    {
+        laserPointer.enabled = false;
+        laserPointer.PointerIn -= HandlePointerIn;
+        laserPointer.PointerOut -= HandlePointerOut;
+
+
+    }
+
+    private void HandlePointerIn(object sender, LaserPointer.PointerEventArgs e)
+    {
+        laserPointer.pointerModel.GetComponent<MeshRenderer>().material.color = Color.red;
+        targetHealth = e.target.gameObject.GetComponent<EnemyHealth>();
+
+        damage = true;
+
+        if (targetHealth)
+        {
+            damage = true;
+        }
+            
+    }
+
+    private void HandlePointerOut(object sender, LaserPointer.PointerEventArgs e)
+    {
+        laserPointer.pointerModel.GetComponent<MeshRenderer>().material.color = laserDefaultColor;
+        targetHealth = null;
+        damage = false;
+    }
+
+
+    void Update () {
+
+        if (damage)
+        {
+            targetHealth.TakeDamage(damagePerFrame);
+        }
+	
+	}
+}
